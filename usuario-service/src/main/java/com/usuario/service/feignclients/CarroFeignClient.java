eb1fd97cf3befe421fcc291d733dc6cb533305bb
Chamo14 <Chamo.uni14@gmail.com>
2022-08-12 03:03:03 -0300

    CB

--- usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.usuario.service.modelos.Carro;
 
-@FeignClient(name = "carro-service")
+@FeignClient(name = "carro-service", url="http://localhost:8080/carro")
 public interface CarroFeignClient {
 	@PostMapping()
 	public Carro save(@RequestBody Carro carro);
